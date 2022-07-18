e32a58cec850f619e862f13c6b15e9b218225523
RinGo1925 <ngsk_bamboo.bulu-1223@hotmai.co.jp>
2022-07-18 23:14:55 +0900

    Indexの編集とIndexcontrollerの追加

---
+++ src/main/java/jp/co/bamboo/infomanager/controller/IndexController.java
@@ -0,0 +1,14 @@
+package jp.co.bamboo.infomanager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class IndexController {
+	@RequestMapping(path="/")
+	public String index(){
+		return "index";
+	}
+
+}
