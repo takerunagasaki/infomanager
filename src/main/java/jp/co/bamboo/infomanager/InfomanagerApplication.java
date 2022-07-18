f3dd1c0f0c5e5e5023983dbca33ca15e677e0d93
takerunagasaki <bamboo.bulu.1994dec@me.com>
2022-07-18 17:23:33 +0900

    farst commit

---
+++ src/main/java/jp/co/bamboo/infomanager/InfomanagerApplication.java
@@ -0,0 +1,13 @@
+package jp.co.bamboo.infomanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfomanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfomanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/co/bamboo/infomanager/InfomanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.bamboo.infomanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfomanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
